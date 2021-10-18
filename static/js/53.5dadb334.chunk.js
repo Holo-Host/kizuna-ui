d1c3cce22967cec5d88796ee609d8ec3777ecff0
Alastair Ong <alastair.ong@holo.host>
2021-10-18 12:21:12 +0100

    new UI

--- static/js/53.5dadb334.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpui=this.webpackJsonpui||[]).push([[53],{283:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var i=e(37),o=e(33),c=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var r=c.closest("ion-content");r&&new Promise((function(n){return Object(o.c)(r,n)})).then((function(){Object(i.f)((function(){return r.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=53.5dadb334.chunk.js.map
